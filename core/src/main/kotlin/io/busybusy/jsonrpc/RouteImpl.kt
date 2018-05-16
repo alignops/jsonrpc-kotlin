c87e91b1010c5f9db676b9b3e4b19743054900de
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2018-05-15 23:17:09 -0600

    Implemented a reflection-based version of RouteSession

---
+++ core/src/main/kotlin/io/busybusy/jsonrpc/RouteImpl.kt
@@ -0,0 +1,77 @@
+package io.busybusy.jsonrpc
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import java.lang.reflect.Type
+import kotlin.reflect.KCallable
+import kotlin.reflect.KType
+import kotlin.reflect.KVisibility
+import kotlin.reflect.full.allSupertypes
+import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.full.valueParameters
+import kotlin.reflect.jvm.javaType
+import kotlin.reflect.jvm.jvmErasure
+
+class HandlerRoute<Parameters : Any, Output : Any>(
+    val handlerFactory: () -> Handler<Parameters, Output>
+) : Route<Parameters, Output> {
+    override fun createSession() = HandlerRouteSession.create(handlerFactory())
+}
+
+/**
+ * {@code RouteSession} implementation that uses on-demand reflection to determine the {@code RouteSession::inputType}
+ * and the {@code RouteSession::outputType}
+ */
+class HandlerRouteSession<Parameters : Any, Output : Any> private constructor(
+    private val handler: Handler<Parameters, Output>,
+    private val method: KCallable<Output>,
+    override val inputType: KType,
+    override val outputType: KType
+) : RouteSession<Parameters, Output> {
+    private val requestType: Type get() = when (inputType.jvmErasure) {
+        Unit::class -> UnitRequest::class.java
+        else -> Types.newParameterizedType(FullRequest::class.java, inputType.javaType)
+    }
+    private val responseType: Type get() = Types.newParameterizedType(SuccessResponse::class.java, outputType.javaType)
+
+    override fun createRequestAdapter(moshi: Moshi): JsonAdapter<RequestWithParameters<Parameters>> = moshi.adapter(requestType)
+
+    override fun createResponse(id: String?, result: Output) = SuccessResponse(id, result)
+    override fun createResponseAdapter(moshi: Moshi): JsonAdapter<ResponseWithResult<Output>> = moshi.adapter(responseType)
+
+    override fun invoke(params: Parameters): Output = method.call(handler, params)
+
+    companion object {
+        /**
+         * Constructs a {@code HandlerRouteSession} with the full typing information determined via the {@code Handler}
+         * interface implementation.
+         */
+        fun <Parameters : Any, Output : Any> create(handler: Handler<Parameters, Output>) = run {
+            val handlerErasure = Handler::class
+            val handlerClass = handler::class
+
+            val types = handlerClass.allSupertypes.first { handlerErasure == it.jvmErasure }.arguments
+            assert(types.size == 2)
+
+            val inputType = types[0].type!!
+            val outputType = types[1].type!!
+
+            /**
+             * This search ensures that the referenced invoke method is the implementation of {@code Handler::invoke}
+             */
+            @Suppress("UNCHECKED_CAST")
+            val method = handlerClass.memberFunctions.first {
+                it.run {
+                    name == "invoke" &&
+                        valueParameters.size == 1 &&
+                        visibility == KVisibility.PUBLIC &&
+                        valueParameters[0].type == inputType &&
+                        returnType == outputType
+                }
+            } as KCallable<Output>
+
+            HandlerRouteSession(handler, method, inputType, outputType)
+        }
+    }
+}
